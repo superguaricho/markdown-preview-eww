23af600a847a08caa41bd22c1f44420853c1177b
numa.tortolero <numa.tortolero@gmail.com>
2026-01-03 17:09:50 -0400

    Fix: Ensure Redcarpet is used for Markdown conversion. The previous implementation failed to invoke the Redcarpet gem, breaking the preview. This commit corrects the Ruby command, ensures the executable is found, and improves process/window handling, fixing the core functionality.

--- markdown-preview-eww.el
+++ markdown-preview-eww.el
@@ -8,7 +8,6 @@
 ;; Package-Revision: 5853f836425c
 ;; Package-Requires: ((emacs "24.4"))
 
-
 ;; This file is not part of GNU Emacs.
 
 ;; The MIT License (MIT)
@@ -35,6 +34,8 @@
 ;; This package provides the realtime markdown preview by eww.
 
 ;;; Code:
+(require 'markdown-mode)
+
 (defvar markdown-preview-eww-process-name "convert-from-md-to-html"
   "Process name of a converter.")
 
@@ -44,8 +45,20 @@
 (defvar markdown-preview-eww-waiting-idling-second 1
   "Seconds of convert waiting")
 
+(defvar *markdown-preview-eww-output-file* nil
+  "The absolute path to the temporary output file for markdown preview.")
+
+(defvar *markdown-preview-eww-preview-buffer* nil
+  "The markdown preview buffer.")
+
+(defvar *markdown-preview-eww-timer* nil
+  "The timer object for markdown preview.")
+
+(defvar markdown-preview-eww-rbin "ruby"
+  "The Ruby binary path.")
+
 (defun markdown-preview-eww-convert-command (output-file-name)
-  (message "Generating convert command for output file: %s" output-file-name)
+  "Return a Ruby command string to convert markdown OUTPUT-FILE-NAME."
   (format "$stderr.reopen($stdout)
 require \"redcarpet\"
 
@@ -55,48 +68,115 @@ while doc = gets(\"\\0\")
   File.write(\"%s\", markdown.render(doc))
 end" output-file-name))
 
-(add-to-list 'exec-path "/home/numa/.rbenv/bin/")
-(add-to-list 'exec-path
-             (file-name-directory
-              (string-trim-right
-               (shell-command-to-string
-                (format "%s which ruby" (executable-find "rbenv"))))))
+(defun markdown-preview-eww-side-window-p (&optional window)
+  "Return non-nil if WINDOW is a side window.
+A side window is identified by the `window-side' window parameter.
+WINDOW defaults to the selected window."
+  (let ((win (or window (selected-window))))
+    (assq 'window-side (window-parameters win))))
+
+(defun markdown-preview-eww-at-right-of (current-buffer other-buffer)
+  "Set OTHER-BUFFER at right of CURRENT-BUFFER."
+  (if (and (get-buffer current-buffer) (get-buffer other-buffer))
+    (with-current-buffer current-buffer
+      (switch-to-buffer current-buffer)
+      (if (> (length (mapcar #'window-buffer (window-list))) 1)
+        (if (markdown-preview-eww-side-window-p)
+          (delete-window)
+          (delete-other-windows)))
+      (display-buffer other-buffer
+        '(display-buffer-in-side-window .
+           ((side . right)
+             (window-width . 0.5)))))))
 
 (defun markdown-preview-eww--do-convert (ofile markdown-preview-mdbuf)
+  "Convert markdown in MARKDOWN-PREVIEW-MDBUF to HTML and save it to OFILE."
   (let* ((doc (with-current-buffer markdown-preview-mdbuf
                 (buffer-substring-no-properties (point-min) (point-max)))))
-    (message "Idle timer fired for markdown to HTML conversion.")
-    (message "Output file: %s" ofile)
     (or (get-process markdown-preview-eww-process-name)
-        (error "No conversion process"))
+      (error "No conversion process"))
     (if (and ofile (get-process markdown-preview-eww-process-name))
-        (progn (message "Converting markdown to HTML...")
-               (process-send-string
-                (get-process markdown-preview-eww-process-name)
-                (concat doc "\0"))
-               (eww-open-file ofile)
-               (kill-process
-                (get-process markdown-preview-eww-process-name)))
-      (message "No output file or conversion process found."))))
+      (progn
+        (process-send-string
+          (get-process markdown-preview-eww-process-name)
+          (concat doc "\0"))
+        (cond ((get-buffer "*eww*")
+                (when (equal markdown-preview-mdbuf (current-buffer))
+                  (other-window 1))
+                (switch-to-buffer "*eww*")
+                (eww-open-file ofile)
+                (markdown-preview-eww-at-right-of markdown-preview-mdbuf "*eww*")
+                )
+          (t
+            (message "no *eww* buffer, creating new one...")
+            (if (> (length (mapcar #'window-buffer (window-list))) 1)
+              (delete-other-windows))
+            (split-window-right)
+            (other-window 1)
+            (eww-open-file ofile)))))))
+
+(defun markdown-preview-eww-process-sentinel (process event)
+  "Sentinel for markdown preview PROCESS with EVENT."
+  (when (memq (process-status process) '(exit signal))
+    (message "Markdown preview process %s ended with event: %s"
+      (process-name process) event)
+    (when *markdown-preview-eww-timer*
+      (cancel-timer *markdown-preview-eww-timer*)
+      (setq *markdown-preview-eww-timer* nil))
+    (setq *markdown-preview-eww-output-file* nil)
+    (setq *markdown-preview-eww-preview-buffer* nil)
+    (and *markdown-preview-eww-output-file*
+      (delete-file *markdown-preview-eww-output-file*))
+    (and (get-buffer "*eww*") (kill-buffer "*eww*"))))
 
 ;;;### autoload
 (defun markdown-preview-eww ()
   "Start a realtime markdown preview."
   (interactive)
-  (when (executable-find "ruby")
+  (and (executable-find markdown-preview-eww-rbin)
+    (equal major-mode 'markdown-mode)
+    (not (get-process markdown-preview-eww-process-name))
     (let* ((process-connection-type nil)
-           (output-file (make-temp-file "markdown-preview-" nil ".html"))
-           (convert-command (markdown-preview-eww-convert-command output-file))
-           (preview-buf (current-buffer)))
+            (output-file (make-temp-file "markdown-preview-" nil ".html"))
+            (convert-command (markdown-preview-eww-convert-command output-file))
+            (preview-buf (current-buffer)))
       (start-process markdown-preview-eww-process-name
-                     nil
-                     (executable-find "ruby")
-                     "-e"
-                     convert-command)
-      (run-with-idle-timer markdown-preview-eww-waiting-idling-second nil
-                           #'(lambda ()
-                               (markdown-preview-eww--do-convert
-                                output-file preview-buf))))))
+        nil
+        (executable-find markdown-preview-eww-rbin)
+        "-e"
+        convert-command)
+
+      (set-process-sentinel
+        (get-process markdown-preview-eww-process-name)
+        #'markdown-preview-eww-process-sentinel)
+
+      (with-current-buffer preview-buf
+        (switch-to-buffer preview-buf)
+        (split-window-right)
+        (other-window 1))
+
+      (setq *markdown-preview-eww-output-file* output-file)
+      (setq *markdown-preview-eww-preview-buffer* preview-buf)
+      (setq *markdown-preview-eww-timer*
+        (run-with-idle-timer
+          markdown-preview-eww-waiting-idling-second
+          t
+          #'(lambda ()
+              (markdown-preview-eww--do-convert
+                *markdown-preview-eww-output-file*
+                *markdown-preview-eww-preview-buffer*)))))))
+(bind-key (kbd "C-c z") 'markdown-preview-eww markdown-mode-map)
+
+;;;### autoload
+(defun markdown-preview-eww-kill ()
+  "Kill the markdown preview process and timer."
+  (interactive)
+  (let ((process (get-process markdown-preview-eww-process-name)))
+    (and process (kill-process process))
+    (and *markdown-preview-eww-timer*
+      (cancel-timer *markdown-preview-eww-timer*))))
+(bind-key (kbd "C-c k") 'markdown-preview-eww-kill markdown-mode-map)
+
 
 (provide 'markdown-preview-eww)
 ;;; markdown-preview-eww.el ends here
